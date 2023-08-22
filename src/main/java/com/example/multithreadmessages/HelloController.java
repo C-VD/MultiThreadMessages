8b413231b069e98d134026105ebac93466669e87
C-VD <vldr@disroot.org>
2023-08-22 07:14:54 +0300

    Works!

--- src/main/java/com/example/multithreadmessages/HelloController.java
+++ src/main/java/com/example/multithreadmessages/HelloController.java
@@ -6,17 +6,17 @@ import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private TextField txt1;
+    private TextField txtCount1;
 
     @FXML
-    private TextField txt2;
+    private TextField txtCount2;
 
     @FXML
     protected void onStart1Btn() {
-        ThreadGenerator.startThread(1, Integer.parseInt(txt1.getText()));
+        ThreadGenerator.startThread(1, Integer.parseInt(txtCount1.getText()));
     }
     @FXML
     protected void onStart2Btn() {
-        ThreadGenerator.startThread(2, Integer.parseInt(txt2.getText()));
+        ThreadGenerator.startThread(2, Integer.parseInt(txtCount2.getText()));
     }
 }
